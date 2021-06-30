58e8e201382fac6d7e06ec694cf27552d3071fbb
Brian Freeman <brian.freeman@rcoffee.io>
2021-06-30 18:25:23 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/r-coffee/score-engine-v2-sdk
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/grpc v1.39.0
+	google.golang.org/protobuf v1.27.1
+)
---
+++ sdk.go
@@ -0,0 +1,47 @@
+package score
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+type ScoreEngineSDKClient struct {
+	stub ScoreServiceClient
+}
+
+// CreateScoreEngine creates a new sdk client
+// host is the hostname of the server
+// pathToCert is the path to the server's public certificate
+// port is the port number the server service is running on
+func CreateScoreEngine(host, pathToCert string, port int) *ScoreEngineSDKClient {
+	var sdk ScoreEngineSDKClient
+	creds, err := credentials.NewClientTLSFromFile(pathToCert, host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// connection timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctx, fmt.Sprintf("%s:%d", host, port), grpc.WithTransportCredentials(creds), grpc.WithBlock())
+	if err != nil {
+		log.Fatal(err)
+	}
+	sdk.stub = NewScoreServiceClient(conn)
+	return &sdk
+}
+
+// Append will write a new row to the table
+func (s *ScoreEngineSDKClient) TennisMatches(tournamentID int64, category, round string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := s.stub.TennisMatches(ctx, &TennisMatchesRequest{TournamentID: tournamentID, Category: category, Round: round})
+	return err
+}
